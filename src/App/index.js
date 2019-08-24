e6f4e40978b0f61699f19a1dbfe2dd12fffa53c8
Roger Cruz <rogercruz@MacBook-Pro-de-Roger.local>
2019-08-23 22:08:39 -0300

    Added material ui top bar and normalize css

--- src/App/index.js
+++ src/App/index.js
@@ -3,13 +3,28 @@ import { HashRouter, Switch, Route } from 'react-router-dom';
 import Detail from '../Views/Detail';
 import Home from '../Views/Home';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 const App = () => (
-    <HashRouter>
-        <Switch>
-            <Route exact path="/" name="Search" component={Home} />
-            <Route exact path="/detail" name="Search" component={Detail} />
-        </Switch>
-    </HashRouter>
+    <div>
+        <AppBar position="static">
+            <Toolbar variant="dense">
+            <Typography variant="h6" color="inherit">
+                Photos
+            </Typography>
+            </Toolbar>
+        </AppBar>
+        
+        
+        <HashRouter>
+            <Switch>
+                <Route exact path="/" name="Search" component={Home} />
+                <Route exact path="/detail" name="Search" component={Detail} />
+            </Switch>
+        </HashRouter>
+    </div>
 );
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import App from './App';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
